6b94a72381c29688d52f5471e0d3be33a55fb065
amnky <amanyadav23421@gmail.com>
2023-11-01 17:01:58 +0530

    testing

--- api/routes/emailrouter.js
+++ api/routes/emailrouter.js
@@ -2,5 +2,6 @@ const express = require("express")
 const router = express.Router()
 const { sendEmail } = require("../controller/emailControllers.js")
 router.post("/sendemail", sendEmail)
+// fuckeing
 
 module.exports = router
